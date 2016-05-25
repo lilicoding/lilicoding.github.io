f95cdb235d3d5734625b4f5b749c08ca32b66b40
Li <joeyli1989@gmail.com>
2016-05-25 20:58:36 +0200

    update scripts

--- RepoSA3/scripts/extract_references.sh
+++ RepoSA3/scripts/extract_references.sh
@@ -2,6 +2,9 @@
 
 PDF_PATH=$1
 
+# pdfx is avaliable at https://github.com/metachris/pdfx
+# pdfx relies on pdfminer2, which is available at https://github.com/metachris/pdfminer
+
 PDF_NAME=`basename $PDF_PATH`
 cp $PDF_PATH $PDF_NAME
 
